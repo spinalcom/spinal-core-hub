ab8fce218a1ddfb0bffaffe8589eb0c8336f0a65
laurent <htd.laurent@gmail.com>
2018-04-23 15:56:14 +0200

    update hub no docker

--- bin/spinalhub.js
+++ bin/spinalhub.js
@@ -1,33 +1,33 @@
 // TODO: fill with other spinalhub params
-var containerized = require('containerized');
+var containerized = require("containerized");
 
-const {
-  exec
-} = require('child_process');
-const path = require('path');
-
+const { exec } = require("child_process");
+const path = require("path");
 
 if (!containerized()) {
+  let browserOrgans = path.resolve("../.browser_organs");
 
-  let browserOrgans = path.resolve('../.browser_organs');
-  let memoryDir = path.resolve('./memory');
+  if (!process.env.SPINALHUB_PORT) {
+    process.env.SPINALHUB_PORT = 7777;
+  }
+  const port = parseInt(process.env.SPINALHUB_PORT);
+  const cmd = `./spinalhub -b ${browserOrgans} -p ${port} -P ${port +
+    1} -q ${port + 2}`;
 
-  exec('docker run -v ' + memoryDir + ':/memory -v ' + browserOrgans + ':/html  -p ' + process.env.SPINALHUB_PORT + ':8888 spinalcom/spinalhub:v3.0.0', (err, stdout, stderr) => {
+  exec(cmd, (err, stdout) => {
     if (err) {
       console.error(`exec error: ${err}`);
       return;
     }
     console.log(stdout);
   });
-
 } else {
   // TODO: allow configuration of ports
-  exec('/spinalhub -b /usr/src/app/.browser_organs', (err, stdout, stderr) => {
+  exec("/spinalhub -b /usr/src/app/.browser_organs", (err, stdout) => {
     if (err) {
       console.error(`exec error: ${err}`);
       return;
     }
     console.log(stdout);
   });
-
 }
